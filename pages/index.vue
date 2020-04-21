24a538c74142a0a633fe8b489f82e1ab869140ac
India Lyons <india.lyons@gmail.com>
2020-04-21 00:50:31 -0400

    Testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="m-16">
-    <h1 class="text-center">Hello, World!</h1>
+  <div class="m-16 text-center">
+    <h1 class="text-2xl mb-3">Hello, World!</h1>
+
+    <div class="underline">Stuff</div>
   </div>
 </template>
