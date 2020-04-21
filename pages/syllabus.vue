7b4bf3e664d43fbc94ff9e89e1c6808a04d1856d
Victor Tolbert <victor.tolbert@gmail.com>
2020-04-21 00:17:37 -0400

    added pdf to syllabus page

--- pages/syllabus.vue
+++ pages/syllabus.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="m-6">
     <h1 class="text-2xl">Syllabus</h1>
+
+      <div class="media-container container mx-auto max-w-4xl" style="position: relative; height: 800px">
+        <iframe
+          style="top: 0;left: 0;width: 100%;height: 100%;border: 0;"
+          src="/syllabus-2019.pdf"
+          width="100%"
+          height="100%"
+        >
+          This browser does not support PDFs. Please download the PDF to view it: Download PDF
+        </iframe>
+      </div>
   </div>
 </template>
+
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.media-container {
+  position: relative;
+  overflow: hidden;
+  padding-top: 26.25%;
+}
+
+.media-container iframe,
+.media-container video,
+.media-container img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
