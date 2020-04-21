6c1f82b9bd29e21e53b2541a41c660625677e17f
Victor Tolbert <victor.tolbert@gmail.com>
2020-04-20 21:26:17 -0400

    add initial site navigation

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+      <nuxt-link to="/syllabus">
+        Syllabus
+      </nuxt-link>
+      <nuxt-link to="/resources">
+        Class Resources
+      </nuxt-link>
+      <nuxt-link to="/contact">
+        Standards
+      </nuxt-link>
+      <nuxt-link to="/contact">
+        Contact
+      </nuxt-link>
+      <nuxt-link to="/portfolio">
+        Portfolio
+      </nuxt-link>
+    </nav>
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
