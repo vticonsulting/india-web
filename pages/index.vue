b16c96ab733f07eed2300dcc4b96c07e200c5ddc
Victor Tolbert <victor.tolbert@gmail.com>
2020-04-22 07:49:47 -0400

    add bitmojis

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="my-4 mx-16 max-w-2xl mx-auto px-6 text-xl">
 
-    <div class="font-body">
+    <div class="font-body" style="background: url(/bitmoji-1.png) no-repeat 100% 0">
       <h2 class="text-2xl mb-4">About me</h2>
 
-      <div class="mb-4">
-        <p> My name is India Lyons.  I am a teacher in sixth-grade mathematics. I have ten years of experience in the educations field. I’ve worked with students from Kindergarten to 8th grade. I’ve been a tutor and after-school counselor and director.</p>
+      <div class="mb-4 pr-32">
+
+        <p class="mb-4"> My name is India Lyons.  I am a teacher in sixth-grade mathematics. I have ten years of experience in the educations field. I’ve worked with students from Kindergarten to 8th grade. I’ve been a tutor and after-school counselor and director.</p>
 
         <p> I have a BA in Political Science with a minor in Economics from Howard University. I am currently a student at Georgia State University as a Masters of Arts in Teaching Middle Grades candidate.</p>
 
@@ -16,7 +17,7 @@
 
       <p class="mb-4">
           All students have a right to an equitable education. I believe in creating a positive environment for all students of different learning styles to flourish and reach their highest learning potential.
-          My classroom is a community that consists of a teacher, students, administration, and parents that work together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community. I believe students can become their own facilitators in the quest of knowledge as well as peer teachers. The is goal is to have a classroom that creates cooperative ownership of the learning process. 
+          My classroom is a community that consists of a teacher, students, administration, and parents that work together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community. I believe students can become their own facilitators in the quest of knowledge as well as peer teachers. The is goal is to have a classroom that creates cooperative ownership of the learning process.
 
       <h2 class="text-2xl mb-4">Class Expectations</h2>
 
