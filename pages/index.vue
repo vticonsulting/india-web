fb9df925018e7bff467b81619cc8ad2313553d58
Victor Tolbert <victor.tolbert@gmail.com>
2020-04-22 17:00:33 -0400

    a little tweaking

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="my-4  mx-16 max-w-3xl mx-auto px-6 text-xl">
+  <div class="my-4 mx-16 mb-12 max-w-3xl mx-auto px-6 text-xl" style="background: url(/bitmoji-3.png) no-repeat 124% 0">
 
-    <div class="font-body" style="background: url(/bitmoji-1.png) no-repeat 100% 0">
+    <div class="font-body">
 
       <!-- Blockquote Component -->
       <blockquote class="mb-8 md:flex-grow md:flex md:flex-col">
@@ -35,33 +35,38 @@
 
       <h2 class="text-2xl mb-4">About me</h2>
 
-      <div class="mb-4 pr-32">
-        <p class="mb-4"> My name is India Lyons.  I am a teacher in sixth-grade mathematics. I have ten years of experience in the educations field. I’ve worked with students from Kindergarten to 8th grade. I’ve been a tutor and after-school counselor and director.</p>
+      <div class="mb-4">
+        <p class="mb-4 pr-56"> My name is India Lyons.  I teach sixth-grade mathematics. For ten years, I’ve worked with students from kindergarten to 8th grade in the classroom, and as a tutor and after-school counselor and director.</p>
 
-        <p> I have a BA in Political Science with a minor in Economics from Howard University. I am currently a student at Georgia State University as a Masters of Arts in Teaching Middle Grades candidate.</p>
+        <p class="mb-4 pr-56">Currently, I'm a student at Georgia State University and I am a candiate for a Masters of Arts in Teaching Middle Grades. I also have a BA in Political Science with a minor in Economics from Howard University.</p>
 
-        <p> This is my 1st year as a Math teacher at Stone Mountain Middle school, and I could not be any happier to work with the best and the brightest students that Dekalb County has to offer.</p>
+        <p>This is my 1st year as a Math teacher at Stone Mountain Middle school, and I could not be any happier to work with the best and the brightest students that Dekalb County has to offer.</p>
       </div>
 
       <h2 class="text-2xl mb-4">Teaching Philosophy</h2>
 
       <p class="mb-4">
-          All students have a right to an equitable education. I believe in creating a positive environment for all students of different learning styles to flourish and reach their highest learning potential.
-          My classroom is a community that consists of a teacher, students, administration, and parents that work together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community. I believe students can become their own facilitators in the quest of knowledge as well as peer teachers. The is goal is to have a classroom that creates cooperative ownership of the learning process.
+          All students have a right to an equitable education. I believe in creating a positive environment for all students of different learning styles to flourish and reach their highest learning potential. My classroom is a community that consists of a teacher, students, administration, and parents working together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community. I believe students can become their own facilitators in the quest of knowledge as well as peer teachers. The goal is to have a classroom that creates cooperative ownership of the learning process.
+      </p>
 
       <h2 class="text-2xl mb-4">Class Expectations</h2>
 
       <p class="mb-4">
-          Please encourage your child to come each day prepared and ready to learn. Students should bring <en>Math Binder</en>, <en>Paper</en>, <en>Pencil</en>, and <en>Chromebook</en> to every class. We expect students to:
-        <ul>
-            <li type="circle">Be respectful and courteous.</li>
-            <li type="circle">Follow all rules, rituals, and routines.</li>
-            <li type="circle">Focus and pay attention in class.</li>
-            <li type="circle">Do not be afraid to ask questions and for help.</li>
-            <li type="circle">Complete and turn in all assignments on time.</li>
-            <li type="circle">Parents, please encourage your child to complete their homework daily.</li>
-        </ul>
-      <p><en>Students are encourage to make up missing assignments immediately.</en></p>
+          Please encourage your child to come each day prepared and ready to learn. Students should bring <em>Math Binder</em>, <em>Paper</em>, <em>Pencil</em>, and <em>Chromebook</em> to every class.
+      </p>
+
+      <p class="mb-4">We expect students to:</p>
+
+      <ul class="list-inside mb-4 ml-4">
+          <li type="circle">Be respectful and courteous.</li>
+          <li type="circle">Follow all rules, rituals, and routines.</li>
+          <li type="circle">Focus and pay attention in class.</li>
+          <li type="circle">Do not be afraid to ask questions and for help.</li>
+          <li type="circle">Complete and turn in all assignments on time.</li>
+          <li type="circle">Parents, please encourage your child to complete their homework daily.</li>
+      </ul>
+
+      <p><em>Students are encourage to make up missing assignments immediately.</em></p>
 
     </div>
   </div>
