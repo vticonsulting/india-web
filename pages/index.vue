0b4e10084c408621dc8ef4ef4d3a9381cb9e7ce9
India Lyons <india.lyons@gmail.com>
2020-04-22 06:35:02 -0400

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,33 @@
     <div class="font-body">
       <h2 class="text-2xl mb-4">About me</h2>
 
-      <p class="mb-4">
-        About me content here...
-      </p>
+      <div class="mb-4">
+        <p> My name is India Lyons.  I am a teacher in sixth-grade mathematics. I have ten years of experience in the educations field. I’ve worked with students from Kindergarten to 8th grade. I’ve been a tutor and after-school counselor and director.</p>
+
+        <p> I have a BA in Political Science with a minor in Economics from Howard University. I am currently a student at Georgia State University as a Masters of Arts in Teaching Middle Grades candidate.</p>
+
+        <p> This is my 1st year as a Math teacher at Stone Mountain Middle school, and I could not be any happier to work with the best and the brightest students that Dekalb County has to offer.</p>
+      </div>
 
       <h2 class="text-2xl mb-4">Teaching Philosophy</h2>
 
       <p class="mb-4">
-          All students have a right to an equitable education. I believe in creating a positive environment for all students of different learning styles to flourish and reach their highest learning potential. My classroom is a community that consists of a teacher, students, administration, and parents that work together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community.
-      </p>
+          All students have a right to an equitable education. I believe in creating a positive environment for all students of different learning styles to flourish and reach their highest learning potential.
+          My classroom is a community that consists of a teacher, students, administration, and parents that work together to maintain order and a positive space. In my class, mistakes are to be made. We learn from our mistakes, discuss them, and plan better strategies as a community. I believe students can become their own facilitators in the quest of knowledge as well as peer teachers. The is goal is to have a classroom that creates cooperative ownership of the learning process. 
 
-      <h2 class="text-2xl mb-4">Experience</h2>
+      <h2 class="text-2xl mb-4">Class Expectations</h2>
 
-      <ul class="mb-4">
-        <li>Wauwatosa West High School - Summer School 2014, 2015</li>
-        <li>Stone Mountain Middle School - 2019 - Present</li>
-      </ul>
+      <p class="mb-4">
+          Please encourage your child to come each day prepared and ready to learn. Students should bring <en>Math Binder</en>, <en>Paper</en>, <en>Pencil</en>, and <en>Chromebook</en> to every class. We expect students to:
+        <ul>
+            <li type="circle">Be respectful and courteous.</li>
+            <li type="circle">Follow all rules, rituals, and routines.</li>
+            <li type="circle">Focus and pay attention in class.</li>
+            <li type="circle">Do not be afraid to ask questions and for help.</li>
+            <li type="circle">Complete and turn in all assignments on time.</li>
+            <li type="circle">Parents, please encourage your child to complete their homework daily.</li>
+        </ul>
+      <p><en>Students are encourage to make up missing assignments immediately.</en></p>
     </div>
   </div>
 </template>
--- pages/standards.vue
+++ pages/standards.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="my-4 mx-16 max-w-3xl mx-auto px-6 font-body">
-    <h1 class="text-2xl">Standards</h1>
+      <div class="media-container container mx-auto max-w-4xl" style="position: relative; height: 800px">
+        <iframe
+          style="top: 0;left: 0;width: 100%;height: 100%;border: 0;"
+          src="/standards.pdf"
+          width="100%"
+          height="100%"
+        >
+        This browser does not support PDFs. Please download the PDF to view it: Download PDF
+        </iframe>
+      </div>
   </div>
 </template>
